38bce0ac631c79022d351d96a585b3f3a0f20f53
tqtensor <victortang212@gmail.com>
2023-05-19 09:11:35 +0700

    chore: add post-install action

--- install_docker.sh
+++ install_docker.sh
@@ -8,7 +8,9 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 sudo apt-get update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo usermod --append --groups docker "$USER"
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
 sudo systemctl enable docker
 printf '\nDocker installed successfully\n\n'
 printf 'Waiting for Docker to start...\n\n'
