ecaefea4cecbba85633b09b44b7c1c68adf26271
tqtensor <victortang212@gmail.com>
2025-01-06 12:38:50 +0100

    feat: add IAM user for Nextcloud

--- resources/providers/aws.py
+++ resources/providers/aws.py
@@ -1,6 +1,10 @@
 import pulumi_aws as aws
 
+# Krypfolio AWS account
 aws_eu_central_1 = aws.Provider(
     "aws-eu-central-1", region="eu-central-1", profile="krypfolio"
 )
 aws_us_east_1 = aws.Provider("aws-us-east-1", region="us-east-1", profile="krypfolio")
+
+# Personnal AWS account
+aws_personal = aws.Provider("aws-personal", region="us-east-1", profile="personal")
