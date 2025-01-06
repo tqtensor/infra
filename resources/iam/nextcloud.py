7422df695579ae29f31308c012573f946e269d25
tqtensor <victortang212@gmail.com>
2025-01-06 12:38:50 +0100

    feat: create Nextcloud IAM user with S3 access and exports

---
+++ resources/iam/nextcloud.py
@@ -0,0 +1,51 @@
+import pulumi
+import pulumi_aws as aws
+
+from resources.providers.aws import aws_personal
+
+OPTS = pulumi.ResourceOptions(
+    protect=True,
+    provider=aws_personal,
+)
+
+
+def create_nextcloud_user():
+    nextcloud_user = aws.iam.User(
+        "nextcloud_user", name="nextcloud_user", force_destroy=True, opts=OPTS
+    )
+
+    nextcloud_s3_policy = aws.iam.Policy(
+        "nextcloud_s3_policy",
+        policy=pulumi.Output.json_dumps(
+            {
+                "Version": "2012-10-17",
+                "Statement": [
+                    {
+                        "Effect": "Allow",
+                        "Action": ["s3:ListBucket"],
+                        "Resource": "arn:aws:s3:::tqtensor-nextcloud-eu",
+                    },
+                    {
+                        "Effect": "Allow",
+                        "Action": ["s3:PutObject", "s3:GetObject", "s3:DeleteObject"],
+                        "Resource": "arn:aws:s3:::tqtensor-nextcloud-eu/*",
+                    },
+                ],
+            }
+        ),
+        opts=OPTS,
+    )
+
+    nextcloud_policy_attachment = aws.iam.UserPolicyAttachment(
+        "nextcloud_policy_attachment",
+        user=nextcloud_user.name,
+        policy_arn=nextcloud_s3_policy.arn,
+        opts=OPTS,
+    )
+
+    nextcloud_access_key = aws.iam.AccessKey(
+        "nextcloud_access_key", user=nextcloud_user.name, opts=OPTS
+    )
+
+    pulumi.export("Nextcloud: access_key_id:", nextcloud_access_key.id)
+    pulumi.export("Nextcloud: secret_access_key:", nextcloud_access_key.secret)
