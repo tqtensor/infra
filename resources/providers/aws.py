cb452e2cb74b1d35d4982be423c42a4fcd56ef15
tqtensor <victortang212@gmail.com>
2025-01-09 11:32:27 +0100

    fix: typo

--- resources/providers/aws.py
+++ resources/providers/aws.py
@@ -6,5 +6,5 @@ aws_eu_central_1 = aws.Provider(
 )
 aws_us_east_1 = aws.Provider("aws-us-east-1", region="us-east-1", profile="krypfolio")
 
-# Personnal AWS account
+# Personal AWS account
 aws_personal = aws.Provider("aws-personal", region="us-east-1", profile="personal")
