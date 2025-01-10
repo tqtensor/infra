0cf6e0d10252c167d4e92f3b11e59282588ff93e
tqtensor <victortang212@gmail.com>
2025-01-10 10:25:51 +0100

    feat: add Bedrock IAM user

--- __main__.py
+++ __main__.py
@@ -1,9 +1,4 @@
 """A Python Pulumi program"""
 
-from resources.ec2.airbyte import create_airbyte_instance
-from resources.ec2.nextcloud import create_nextcloud_instance
-from resources.iam.nextcloud import create_nextcloud_user
-
-create_airbyte_instance()
-create_nextcloud_instance()
-create_nextcloud_user()
+import resources.ec2  # noqa
+import resources.iam  # noqa
---
+++ resources/ec2/__init__.py
@@ -0,0 +1,5 @@
+from .airbyte import create_airbyte_instance
+from .nextcloud import create_nextcloud_instance
+
+create_airbyte_instance()
+create_nextcloud_instance()
