28d00c4cf25ffeda5b8947a58c9fc7b183da94c0
tqtensor <victortang212@gmail.com>
2025-01-17 17:25:24 +0100

    feat: import all domains from squarespace

--- resources/constants.py
+++ resources/constants.py
@@ -6,8 +6,28 @@ import pulumi
 import pulumi_cloudflare as cloudflare
 
 # Cloudflare
+krypfolio_com = cloudflare.Zone.get(
+    "krypfolio_com",
+    id="04f429cac6262bf28db5731a84fde86a",
+    account_id=pulumi.Config().require("accountId"),
+)
+mservice_dev = cloudflare.Zone.get(
+    "mservice_dev",
+    id="2a103726472fbfdf5fe1a6ba2121c723",
+    account_id=pulumi.Config().require("accountId"),
+)
 tqtensor_com = cloudflare.Zone.get(
     "tqtensor_com",
     id="1fa667eb57e3e586d4f0bd8b6cd2e7ad",
     account_id=pulumi.Config().require("accountId"),
 )
+unifai_dev = cloudflare.Zone.get(
+    "unifai_dev",
+    id="e37a9e60005d57df4859fa4817c8128a",
+    account_id=pulumi.Config().require("accountId"),
+)
+
+pulumi.export("Cloudflare: krypfolio.com", krypfolio_com.id)
+pulumi.export("Cloudflare: mservice.dev", mservice_dev.id)
+pulumi.export("Cloudflare: tqtensor.com", tqtensor_com.id)
+pulumi.export("Cloudflare: unifai.dev", unifai_dev.id)
