a2873bb927e69a1be1ff4912bfe108d768b75f2f
tqtensor <victortang212@gmail.com>
2025-01-30 15:21:49 +0100

    feat: add workstation vm in eu

---
+++ resources/ec2/keypair.py
@@ -0,0 +1,13 @@
+import pulumi_aws as aws
+
+from resources.utils import get_options
+
+OPTS = get_options(profile="krypfolio", region="eu-central-1", type="resource")
+
+
+krypfolio_eu_central_1_key_pair = aws.ec2.KeyPair(
+    "krypfolio_eu_central_1_key_pair",
+    key_name="infra",
+    public_key="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDbnm55mNBsS3Ackocgh2jv79s5Ep4MsTxCD8OnG6QXIk6QvCATYHxHnhXmcokwpjMPiSUJdhbfVTZCq05jCDjEgSFAZRJSOopzmCS0nzpWcdlhvnLRGZinH8zTBwK4AX/aPMfb301fICZf0Z7pZFA4rNTjSaDs591aIlZWC9sJIPJvaglHaFSTdxCyFoX0JFVIcHXxiE0PP1i3ECYAePtlERJtVQ0UkpV5B4XaDrE+Nglp3iaXRDUHYJIirqfVkT4WABlFJu0Em7UzGghOCqyeoSqEm01H+txUs8065EUQ+7MZArSMGXlNr7YVWrWY2fppxDNQ2XSyOer5NSz5y05t infra\n",
+    opts=OPTS,
+)
