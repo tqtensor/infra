9ae3e0a49392699f5092758ac9014c1671d1981e
tqtensor <victortang212@gmail.com>
2025-01-31 19:35:59 +0100

    feat: add record for n8n domain

--- resources/cloudflare/record/tqtensor_com.py
+++ resources/cloudflare/record/tqtensor_com.py
@@ -1,7 +1,7 @@
 import pulumi_cloudflare as cloudflare
 
 from resources.constants import tqtensor_com
-from resources.ec2 import nextcloud_instance
+from resources.ec2 import n8n_eu_central_1_instance, nextcloud_instance
 from resources.utils import get_options
 
 OPTS = get_options(provider="cloudflare")
@@ -13,6 +13,16 @@ drive_tqtensor_com = cloudflare.Record(
     ttl=1,
     type="A",
     content=nextcloud_instance.public_ip,
+    zone_id=tqtensor_com.id,
+    opts=OPTS,
+)
+
+n8n_tqtensor_com = cloudflare.Record(
+    "n8n_tqtensor_com",
+    name="n8n",
+    ttl=1,
+    type="A",
+    content=n8n_eu_central_1_instance.public_ip,
     zone_id=tqtensor_com.id,
     opts=OPTS,
 )
