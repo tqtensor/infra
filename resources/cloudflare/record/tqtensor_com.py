8148f3e4c69b6c310e5e893ab4c61747b02220be
tqtensor <victortang212@gmail.com>
2025-02-09 21:18:29 +0100

    feat: remove n8n instance

--- resources/cloudflare/record/tqtensor_com.py
+++ resources/cloudflare/record/tqtensor_com.py
@@ -2,11 +2,9 @@ import pulumi_cloudflare as cloudflare
 
 from resources.constants import tqtensor_com
 from resources.utils import get_options
-from resources.vm import n8n_eu_central_1_instance, nextcloud_instance
+from resources.vm import nextcloud_instance
 
-OPTS = get_options(
-    provider="cloudflare",
-)
+OPTS = get_options(provider="cloudflare", protect=False)
 
 
 drive_tqtensor_com = cloudflare.Record(
@@ -15,16 +13,6 @@ drive_tqtensor_com = cloudflare.Record(
     ttl=1,
     type="A",
     content=nextcloud_instance.public_ip,
-    zone_id=tqtensor_com.id,
-    opts=OPTS,
-)
-
-n8n_tqtensor_com = cloudflare.Record(
-    "n8n_tqtensor_com",
-    name="n8n",
-    ttl=1,
-    type="A",
-    content=n8n_eu_central_1_instance.public_ip,
     zone_id=tqtensor_com.id,
     opts=OPTS,
 )
--- resources/vm/instance/__init__.py
+++ resources/vm/instance/__init__.py
@@ -1,4 +1,3 @@
 from .n8n_ap_southeast_1 import *  # noqa
-from .n8n_eu_central_1 import *  # noqa
 from .nextcloud import *  # noqa
 from .workstation import *  # noqa
