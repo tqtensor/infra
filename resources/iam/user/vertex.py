acd11fd2bd2ef5323845ecd09a6286f8403c234c
tqtensor <victortang212@gmail.com>
2025-02-09 18:40:08 +0100

    feat: connect k8s sa with gcp sa

--- resources/iam/user/vertex.py
+++ resources/iam/user/vertex.py
@@ -1,18 +1,20 @@
 import pulumi
 import pulumi_gcp as gcp
 
-from resources.providers import gcp_pixelml_europe_central_2
+from resources.providers import gcp_pixelml_europe_west_4
 from resources.utils import get_options
 
 OPTS = get_options(
-    profile="pixelml", region="europe-central-2", type="resource", provider="gcp"
+    profile="pixelml",
+    region="europe-west-4",
+    type="resource",
+    provider="gcp",
 )
 
 
 vertex_sa = gcp.serviceaccount.Account(
     "vertex_sa",
-    account_id="vertex-sa-europe-central-2",
-    display_name="Vertex AI Service Account",
+    account_id="vertex-sa-europe-west-4",
 )
 
 roles = [
@@ -22,7 +24,7 @@ roles = [
 for role in roles:
     gcp.projects.IAMMember(
         f"vertex_sa_{role}",
-        project=gcp_pixelml_europe_central_2.project,
+        project=gcp_pixelml_europe_west_4.project,
         role=role,
         member=vertex_sa.email.apply(lambda email: f"serviceAccount:{email}"),
     )
