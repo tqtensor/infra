b0e0fdb4bb6d6d638a508b25d48674a7d7c18627
tqtensor <victortang212@gmail.com>
2025-02-10 07:24:11 +0100

    feat: add tls for n8n

--- resources/k8s/helm/n8n.py
+++ resources/k8s/helm/n8n.py
@@ -4,8 +4,9 @@ import pulumi
 import pulumi_kubernetes as k8s
 import yaml
 
+from resources.cloudflare import n8n_origin_ca_cert, n8n_private_key
 from resources.providers import gcp_pixelml_europe_west_4
-from resources.utils import get_options
+from resources.utils import encode_tls_secret_data, get_options
 
 OPTS = get_options(
     profile="pixelml",
@@ -16,6 +17,15 @@ OPTS = get_options(
 
 
 n8n_ns = k8s.core.v1.Namespace("n8n_ns", metadata={"name": "n8n"}, opts=OPTS)
+
+n8n_tls_secret = k8s.core.v1.Secret(
+    "n8n_tls_secret",
+    metadata={"name": "n8n-tls-secret", "namespace": n8n_ns.metadata["name"]},
+    data=pulumi.Output.all(
+        n8n_origin_ca_cert.certificate, n8n_private_key.private_key_pem
+    ).apply(lambda args: encode_tls_secret_data(args[0], args[1])),
+    opts=OPTS,
+)
 
 values_file_path = os.path.join(os.path.dirname(__file__), "values", "n8n.yaml")
 with open(values_file_path, "r") as f:
