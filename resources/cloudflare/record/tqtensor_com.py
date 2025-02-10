d2a0d5930ac44c1518c06a41514fa7820392b992
tqtensor <victortang212@gmail.com>
2025-02-10 20:14:53 +0100

    feat: connect psql with n8n deployment

--- resources/cloudflare/record/tqtensor_com.py
+++ resources/cloudflare/record/tqtensor_com.py
@@ -2,10 +2,20 @@ import pulumi_cloudflare as cloudflare
 
 from resources.constants import tqtensor_com
 from resources.utils import get_options
-from resources.vm import nginx_ip_europe_west_4
+from resources.vm import nextcloud_instance, nginx_ip_europe_west_4
 
 OPTS = get_options(provider="cloudflare")
 
+
+drive_tqtensor_com = cloudflare.Record(
+    "drive_tqtensor_com",
+    name="drive",
+    ttl=1,
+    type="A",
+    content=nextcloud_instance.public_ip,
+    zone_id=tqtensor_com.id,
+    opts=OPTS,
+)
 
 n8n_tqtensor_com = cloudflare.Record(
     "n8n_tqtensor_com",
