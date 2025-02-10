d2cae564e020e045f4ca0de94319e31da10a100e
tqtensor <victortang212@gmail.com>
2025-02-10 07:24:11 +0100

    feat: configure tls for n8n.tqtensor.com ingress

--- resources/cloudflare/record/tqtensor_com.py
+++ resources/cloudflare/record/tqtensor_com.py
@@ -2,7 +2,7 @@ import pulumi_cloudflare as cloudflare
 
 from resources.constants import tqtensor_com
 from resources.utils import get_options
-from resources.vm import nextcloud_instance
+from resources.vm import nextcloud_instance, nginx_ip_europe_west_4
 
 OPTS = get_options(provider="cloudflare", protect=False)
 
@@ -14,6 +14,17 @@ drive_tqtensor_com = cloudflare.Record(
     type="A",
     content=nextcloud_instance.public_ip,
     zone_id=tqtensor_com.id,
+    opts=OPTS,
+)
+
+n8n_tqtensor_com = cloudflare.Record(
+    "n8n_tqtensor_com",
+    name="n8n",
+    ttl=1,
+    type="A",
+    content=nginx_ip_europe_west_4.address,
+    zone_id=tqtensor_com.id,
+    proxied=True,
     opts=OPTS,
 )
 
