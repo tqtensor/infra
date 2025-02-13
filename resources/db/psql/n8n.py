6ddbe230782652a2644360a7963e4adb19efcc28
tqtensor <victortang212@gmail.com>
2025-02-13 12:00:31 +0100

    feat: generate and persist n8n db passwords via pulumi helper

--- resources/db/psql/n8n.py
+++ resources/db/psql/n8n.py
@@ -2,12 +2,12 @@ import os
 
 import pulumi
 import pulumi_postgresql as postgresql
-from sopsy import Sops
 
 from resources.db.rds import (
     krypfolio_eu_central_1_rds_cluster_instance,
     krypfolio_eu_central_1_rds_credentials,
 )
+from resources.utils import fill_in_password
 
 postgres_provider = postgresql.Provider(
     "postgres_provider",
@@ -20,26 +20,23 @@ postgres_provider = postgresql.Provider(
 
 opts = pulumi.ResourceOptions(provider=postgres_provider)
 
-credentials_file_path = os.path.join(os.path.dirname(__file__), "credentials.yaml")
-sops = Sops(credentials_file_path)
-try:
-    credentials = sops.decrypt()
-except Exception as e:
-    pulumi.log.error(f"Failed to decrypt {credentials_file_path}: {e}")
-    raise
 
+def create_db_and_user(username: str):
+    credentials_file_path = os.path.join(os.path.dirname(__file__), "credentials.yaml")
+    credentials = fill_in_password(
+        encrypted_yaml=credentials_file_path, value_path=f"roles.{username}.password"
+    )["roles"]
 
-def create_db_and_user(username: str, credentials: dict):
     db = postgresql.Database(
-        f"n8n_{username}db",
+        f"n8n_{username}_db",
         name=f"n8n_{username}_db",
         opts=opts,
     )
 
     role = postgresql.Role(
         f"n8n_{username}_user",
-        name=credentials["dolphin"]["username"],
-        password=credentials["dolphin"]["password"],
+        name=f"n8n_{username}_user",
+        password=credentials[username]["password"],
         login=True,
         opts=opts,
     )
@@ -56,5 +53,5 @@ def create_db_and_user(username: str, credentials: dict):
 
 
 n8n_dolphin_db, n8n_dolphin_user, n8n_dolphin_grant_privileges = create_db_and_user(
-    username="dolphin", credentials=credentials
+    username="dolphin"
 )
