b5c029c0ec2802d6dd80ea1b1bf932ae830bf9a5
tqtensor <victortang212@gmail.com>
2025-03-01 10:59:56 +0000

    feat: remove vss chart

--- resources/k8s/helm/__init__.py
+++ resources/k8s/helm/__init__.py
@@ -3,6 +3,5 @@ from .litellm import *  # noqa
 from .n8n import *  # noqa
 from .n8n_grokking import *  # noqa
 from .nginx import *  # noqa
-from .vss import *  # noqa
 
 # from .n8n_workshop import *  # noqa
