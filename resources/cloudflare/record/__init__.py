3d7f996b375491f06a69f64a3ebc9f7b637a36fb
tqtensor <victortang212@gmail.com>
2025-03-08 21:52:49 +0000

    feat: remove n8n resources for workshop

--- resources/cloudflare/record/__init__.py
+++ resources/cloudflare/record/__init__.py
@@ -1,3 +1,2 @@
 from .tqtensor_com import *  # noqa
-from .tqtensor_com_workshop import *  # noqa
 from .unifai_dev import *  # noqa
--- resources/k8s/helm/__init__.py
+++ resources/k8s/helm/__init__.py
@@ -4,5 +4,3 @@ from .n8n import *  # noqa
 from .n8n_grokking import *  # noqa
 from .nginx import *  # noqa
 from .tei import *  # noqa
-
-# from .n8n_workshop import *  # noqa
