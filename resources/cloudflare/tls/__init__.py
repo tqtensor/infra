d37fdc67db033740b61fb6ba46489267ddc69eac
tqtensor <victortang212@gmail.com>
2025-03-08 21:52:49 +0000

    feat: remove n8n workshop infra resources and participant data

--- resources/cloudflare/tls/__init__.py
+++ resources/cloudflare/tls/__init__.py
@@ -2,5 +2,4 @@ from .airbyte_tqtensor_com import *  # noqa
 from .grokking_tqtensor_com import *  # noqa
 from .litellm_unifai_dev import *  # noqa
 from .n8n_tqtensor_com import *  # noqa
-from .n8n_tqtensor_com_workshop import *  # noqa
 from .tei_unifai_dev import *  # noqa
--- resources/db/psql/__init__.py
+++ resources/db/psql/__init__.py
@@ -1,4 +1,3 @@
 from .airbyte import *  # noqa
 from .kb import *  # noqa
 from .n8n import *  # noqa
-from .n8n_workshop import *  # noqa
