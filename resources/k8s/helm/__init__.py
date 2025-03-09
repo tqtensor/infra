9975c8d6efecc370afb3d6b32a27f87033527891
tqtensor <victortang212@gmail.com>
2025-03-09 18:07:16 +0000

    refactor: remove asia-east-1 TEI and networking resources

--- resources/k8s/helm/__init__.py
+++ resources/k8s/helm/__init__.py
@@ -3,4 +3,3 @@ from .litellm import *  # noqa
 from .n8n import *  # noqa
 from .n8n_grokking import *  # noqa
 from .nginx import *  # noqa
-from .tei import *  # noqa
--- resources/k8s/providers.py
+++ resources/k8s/providers.py
@@ -1,15 +1,7 @@
 import pulumi_kubernetes as k8s
 
-from resources.k8s.cluster.gke import (
-    auto_pilot_asia_east_1_cluster,
-    auto_pilot_eu_west_4_cluster,
-)
+from resources.k8s.cluster.gke import auto_pilot_eu_west_4_cluster
 from resources.utils import create_kubeconfig
-
-k8s_provider_auto_pilot_asia_east_1 = k8s.Provider(
-    "k8s_provider_auto_pilot_asia_east_1",
-    kubeconfig=create_kubeconfig(cluster=auto_pilot_asia_east_1_cluster),
-)
 
 k8s_provider_auto_pilot_eu_west_4 = k8s.Provider(
     "k8s_provider_auto_pilot_eu_west_4",
