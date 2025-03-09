c0b85482dc86b9c0dcfdadea9b06270bf8483f35
tqtensor <victortang212@gmail.com>
2025-03-09 18:07:16 +0000

    feat: deactivate HuggingFace TEI

--- resources/cloudflare/tls/__init__.py
+++ resources/cloudflare/tls/__init__.py
@@ -2,4 +2,3 @@ from .airbyte_tqtensor_com import *  # noqa
 from .grokking_tqtensor_com import *  # noqa
 from .litellm_unifai_dev import *  # noqa
 from .n8n_tqtensor_com import *  # noqa
-from .tei_unifai_dev import *  # noqa
--- resources/k8s/cluster/gke/__init__.py
+++ resources/k8s/cluster/gke/__init__.py
@@ -1,5 +1,1 @@
-from .auto_pilot_asia_east_1 import *  # noqa
 from .auto_pilot_eu_west_4 import *  # noqa
-
-# from .asia_east_1 import *  # noqa
-# from .federated_eu_west_1 import *  # noqa
