f0923da828c8354a10d51f46fa1b8b209e663028
tqtensor <victortang212@gmail.com>
2025-03-12 21:16:29 +0000

    feat: add JupyterHub deployment

--- resources/cloudflare/tls/__init__.py
+++ resources/cloudflare/tls/__init__.py
@@ -1,4 +1,5 @@
 from .airbyte_tqtensor_com import *  # noqa
 from .grokking_tqtensor_com import *  # noqa
+from .jupyterhub_tqtensor_com import *  # noqa
 from .litellm_tqtensor_com import *  # noqa
 from .n8n_tqtensor_com import *  # noqa
--- resources/db/psql/__init__.py
+++ resources/db/psql/__init__.py
@@ -1,4 +1,5 @@
 from .airbyte import *  # noqa
+from .jupyterhub import *  # noqa
 from .kb import *  # noqa
 from .litellm import *  # noqa
 from .n8n import *  # noqa
