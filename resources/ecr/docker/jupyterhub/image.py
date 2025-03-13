5800bd720e52a6f76c246b09e80af7ce8d4d9343
tqtensor <victortang212@gmail.com>
2025-03-13 20:58:20 +0000

    feat: add JupyterHub Docker image build and registry integration

---
+++ resources/ecr/docker/jupyterhub/image.py
@@ -0,0 +1,51 @@
+import datetime as dt
+import pathlib
+
+import pulumi_docker_build as docker_build
+from pulumi import Output
+
+from resources.ecr.registry import pixelml_eu_west_4_registry
+from resources.providers import gcp_pixelml_eu_west_4
+from resources.utils import create_docker_config
+
+_ = Output.all(gcp_pixelml_eu_west_4.region).apply(
+    lambda args: create_docker_config(
+        provider="gcp", server=args[0] + "-docker.pkg.dev"
+    )
+)
+
+current_file_path = pathlib.Path(__file__).resolve()
+
+image_tag = dt.datetime.now(tz=dt.timezone.utc).strftime("%Y%m%d%H%M%S")
+
+jupyterhub_image = docker_build.Image(
+    "jupyterhub_image",
+    tags=[
+        Output.concat(
+            gcp_pixelml_eu_west_4.region,
+            "-docker.pkg.dev/",
+            gcp_pixelml_eu_west_4.project,
+            "/",
+            pixelml_eu_west_4_registry.repository_id,
+            f"/jupyterhub:{image_tag}",
+        ),
+        Output.concat(
+            gcp_pixelml_eu_west_4.region,
+            "-docker.pkg.dev/",
+            gcp_pixelml_eu_west_4.project,
+            "/",
+            pixelml_eu_west_4_registry.repository_id,
+            "/jupyterhub:latest",
+        ),
+    ],
+    context=docker_build.BuildContextArgs(
+        location=str(current_file_path.parent),
+    ),
+    dockerfile=docker_build.DockerfileArgs(
+        location=str(current_file_path.parent / "Dockerfile"),
+    ),
+    platforms=[
+        docker_build.Platform.LINUX_AMD64,
+    ],
+    push=True,
+)
