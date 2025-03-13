db8f5c2b4d06f9dcea0df276c20d63fc76858818
tqtensor <victortang212@gmail.com>
2025-03-13 19:55:53 +0000

    feat: add RDS backup

--- resources/db/psql/providers.py
+++ resources/db/psql/providers.py
@@ -1,12 +1,15 @@
 import pulumi_postgresql as postgresql
 
-from resources.db.rds import krp_ec1_rds_cluster_instance, krp_ec1_rds_credentials
+from resources.db.rds import (
+    krp_eu_central_1_rds_cluster_instance,
+    krp_eu_central_1_rds_credentials,
+)
 
 krp_ec1_postgres_provider = postgresql.Provider(
     "krp_ec1_postgres_provider",
-    host=krp_ec1_rds_cluster_instance.endpoint,
+    host=krp_eu_central_1_rds_cluster_instance.endpoint,
     port=5432,
-    username=krp_ec1_rds_credentials["username"],
-    password=krp_ec1_rds_credentials["password"],
+    username=krp_eu_central_1_rds_credentials["username"],
+    password=krp_eu_central_1_rds_credentials["password"],
     superuser=False,
 )
