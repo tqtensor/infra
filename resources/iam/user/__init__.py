9fb65e1d322709d1b691b9ec59f1f61d102c89af
tqtensor <victortang212@gmail.com>
2025-03-25 20:00:49 +0000

    feat: add GCS buckets and HuggingFace service account IAM

--- resources/iam/user/__init__.py
+++ resources/iam/user/__init__.py
@@ -1,5 +1,6 @@
 from .arq import *  # noqa
 from .bedrock import *  # noqa
 from .gdrive import *  # noqa
+from .huggingface import *  # noqa
 from .nextcloud import *  # noqa
 from .vertex import *  # noqa
