d410e3a7f654b9423bc1e17286f71602c319df2e
tqtensor <victortang212@gmail.com>
2025-03-26 16:02:50 +0000

    feat: remove HuggingFace image, not working

--- resources/ecr/docker/__init__.py
+++ resources/ecr/docker/__init__.py
@@ -1,3 +1,2 @@
 from .devcontainer import *  # noqa
-from .huggingface import *  # noqa
 from .jupyterhub import *  # noqa
--- resources/iam/user/__init__.py
+++ resources/iam/user/__init__.py
@@ -1,6 +1,5 @@
 from .arq import *  # noqa
 from .bedrock import *  # noqa
 from .gdrive import *  # noqa
-from .huggingface import *  # noqa
 from .nextcloud import *  # noqa
 from .vertex import *  # noqa
