bfd7029434bd32c189e55174b2824beb201c39b9
tqtensor <victortang212@gmail.com>
2025-03-27 06:29:48 +0000

    feat: add Cloud Run service for whisper diarization

---
+++ resources/serverless/cloudrun/__init__.py
@@ -0,0 +1,1 @@
+from .whisper_diarization import *  # noqa
---
+++ resources/serverless/cloudrun/whisper_diarization.py
@@ -0,0 +1,90 @@
+import pulumi_gcp as gcp
+from pulumi import Output
+
+from resources.ecr import whisper_diarization_image_uri
+from resources.providers import gcp_pixelml_us_central_1
+from resources.utils import get_options
+
+OPTS = get_options(
+    profile="pixelml",
+    region="us-central-1",
+    type="resource",
+    provider="gcp",
+)
+
+
+whisper_diarization = gcp.cloudrun.Service(
+    "whisper_diarization",
+    location=Output.all(
+        gcp_pixelml_us_central_1.region,
+    ).apply(lambda args: args[0]),
+    metadata=Output.all(
+        gcp_pixelml_us_central_1.project,
+    ).apply(
+        lambda args: {
+            "namespace": args[0],
+        }
+    ),
+    name="whisper-diarization",
+    project=Output.all(
+        gcp_pixelml_us_central_1.project,
+    ).apply(lambda args: args[0]),
+    template=Output.all(whisper_diarization_image_uri).apply(
+        lambda args: {
+            "metadata": {
+                "annotations": {
+                    "autoscaling.knative.dev/maxScale": "5",
+                    "run.googleapis.com/client-name": "gcloud",
+                    "run.googleapis.com/cpu-throttling": "false",
+                    "run.googleapis.com/execution-environment": "gen2",
+                    "run.googleapis.com/gpu-zonal-redundancy-disabled": "true",
+                    "run.googleapis.com/startup-cpu-boost": "true",
+                },
+                "labels": {
+                    "run.googleapis.com/startupProbeType": "Default",
+                },
+            },
+            "spec": {
+                "container_concurrency": 80,
+                "containers": [
+                    {
+                        "image": args[0],
+                        "ports": [
+                            {
+                                "container_port": 5000,
+                                "name": "http1",
+                            }
+                        ],
+                        "resources": {
+                            "limits": {
+                                "cpu": "4",
+                                "memory": "16Gi",
+                                "nvidia.com/gpu": "1",
+                            },
+                        },
+                        "startup_probe": {
+                            "failure_threshold": 1,
+                            "period_seconds": 240,
+                            "tcp_socket": {
+                                "port": 5000,
+                            },
+                            "timeout_seconds": 240,
+                        },
+                    }
+                ],
+                "node_selector": {
+                    "run.googleapis.com/accelerator": "nvidia-l4",
+                },
+                "service_account_name": "859438098239-compute@developer.gserviceaccount.com",
+                "timeout_seconds": 300,
+            },
+        }
+    ),
+    traffics=[
+        {
+            "latest_revision": True,
+            "percent": 100,
+        }
+    ],
+    opts=OPTS,
+)
