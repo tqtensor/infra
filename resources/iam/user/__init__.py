8c6df7bb3a68c22842868f9bcfafc88dd1fa0df4
tqtensor <victortang212@gmail.com>
2025-04-07 08:55:46 +0000

    feat: add IAM user and S3 access for fast backups

--- resources/iam/user/__init__.py
+++ resources/iam/user/__init__.py
@@ -1,6 +1,7 @@
 from .arq import *  # noqa
 from .bedrock import *  # noqa
 from .cloudrun import *  # noqa
+from .fast import *  # noqa
 from .gdrive import *  # noqa
 from .nextcloud import *  # noqa
 from .vertex import *  # noqa
