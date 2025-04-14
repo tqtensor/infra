3e4822c2866a59352a67576b3500cee6f5b7ae57
tqtensor <victortang212@gmail.com>
2025-04-14 00:23:50 +0000

    chore: remove grokking n8n resources and jupyterhub git hook

--- resources/cloudflare/tls/__init__.py
+++ resources/cloudflare/tls/__init__.py
@@ -1,5 +1,4 @@
 from .airbyte_tqtensor_com import *  # noqa
-from .grokking_tqtensor_com import *  # noqa
 from .jupyterhub_tqtensor_com import *  # noqa
 from .litellm_tqtensor_com import *  # noqa
 from .n8n_tqtensor_com import *  # noqa
--- resources/k8s/helm/__init__.py
+++ resources/k8s/helm/__init__.py
@@ -2,5 +2,4 @@ from .airbyte import *  # noqa
 from .jupyterhub import *  # noqa
 from .litellm import *  # noqa
 from .n8n import *  # noqa
-from .n8n_grokking import *  # noqa
 from .nginx import *  # noqa
