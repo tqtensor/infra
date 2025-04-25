92e87a660423bffc2e52cf326bdece1d3c21c8d5
tqtensor <victortang212@gmail.com>
2025-04-25 15:23:11 +0000

    feat: add GCP workstation and export VM IP addresses

--- resources/vm/instance/workstation.py
+++ resources/vm/instance/workstation.py
@@ -9,9 +9,9 @@ OPTS = get_options(
 )
 
 
-workstation_gpu_firewall = gcp.compute.Firewall(
-    "workstation_gpu_firewall",
-    name="workstation-gpu-firewall",
+workstation_firewall = gcp.compute.Firewall(
+    "workstation_firewall",
+    name="workstation-firewall",
     network="default",
     allows=[
         {
@@ -23,22 +23,20 @@ workstation_gpu_firewall = gcp.compute.Firewall(
     opts=OPTS,
 )
 
-boot_image = gcp.compute.get_image(
-    family="common-cu123", project="deeplearning-platform-release"
-)
+boot_image = gcp.compute.get_image(family="debian-11", project="debian-cloud")
 
-workstation_gpu_ip = gcp.compute.Address(
-    "workstation_gpu_ip",
-    name="workstation-gpu-external-ip",
+workstation_ip = gcp.compute.Address(
+    "workstation_ip",
+    name="workstation-external-ip",
     address_type="EXTERNAL",
     region="us-east1",
     opts=OPTS,
 )
 
-workstation_gpu_instance = gcp.compute.Instance(
-    "workstation_gpu_instance",
-    name="workstation-gpu-instance",
-    machine_type="g2-standard-12",
+workstation_instance = gcp.compute.Instance(
+    "workstation_instance",
+    name="workstation-instance",
+    machine_type="e2-highcpu-32",
     zone="us-east1-c",
     boot_disk=gcp.compute.InstanceBootDiskArgs(
         initialize_params=gcp.compute.InstanceBootDiskInitializeParamsArgs(
@@ -51,20 +49,17 @@ workstation_gpu_instance = gcp.compute.Instance(
             network="default",
             access_configs=[
                 {
-                    "nat_ip": workstation_gpu_ip.address,
+                    "nat_ip": workstation_ip.address,
                     "network_tier": "PREMIUM",
                 }
             ],
         )
     ],
-    scheduling=gcp.compute.InstanceSchedulingArgs(
-        on_host_maintenance="TERMINATE", automatic_restart=True
-    ),
+    scheduling=gcp.compute.InstanceSchedulingArgs(automatic_restart=True),
     metadata={
-        "install-nvidia-driver": "true",
         "ssh-keys": pixelml_gcp_us_east_1_key_pair.metadata["ssh-keys"],
     },
     opts=OPTS,
 )
 
-pulumi.export("VM: workstation_gpu: IP", workstation_gpu_ip.address)
+pulumi.export("VM: workstation: IP", workstation_ip.address)
