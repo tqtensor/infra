f88eb8a213c4e4626806e7378a5cac8a978e81cd
tqtensor <victortang212@gmail.com>
2025-05-02 15:39:19 +0000

    feat: add VSS A100 instance and global firewall for eu-west-4

--- resources/vm/instance/vss.py
+++ resources/vm/instance/vss.py
@@ -2,7 +2,7 @@ import pulumi
 import pulumi_gcp as gcp
 
 from resources.utils import get_options
-from resources.vm.keypair import pixelml_gcp_us_east_1_key_pair
+from resources.vm.keypair import pixelml_gcp_eu_west_4_key_pair
 
 OPTS = get_options(
     profile="pixelml", region="us-east-1", type="resource", provider="gcp"
@@ -13,19 +13,19 @@ boot_image = gcp.compute.get_image(
     family="common-cu123", project="deeplearning-platform-release"
 )
 
-workstation_gpu_ip = gcp.compute.Address(
-    "workstation_gpu_ip",
-    name="workstation-gpu-external-ip",
+vss_ip = gcp.compute.Address(
+    "vss_ip",
+    name="vss-external-ip",
     address_type="EXTERNAL",
-    region="us-east1",
+    region="europe-west4",
     opts=OPTS,
 )
 
-workstation_gpu_instance = gcp.compute.Instance(
-    "workstation_gpu_instance",
-    name="workstation-gpu-instance",
-    machine_type="g2-standard-12",
-    zone="us-east1-c",
+vss_instance = gcp.compute.Instance(
+    "vss_instance",
+    name="vss-instance",
+    machine_type="a2-highgpu-4g",
+    zone="europe-west4-a",
     boot_disk=gcp.compute.InstanceBootDiskArgs(
         initialize_params=gcp.compute.InstanceBootDiskInitializeParamsArgs(
             image=boot_image.self_link,
@@ -37,7 +37,7 @@ workstation_gpu_instance = gcp.compute.Instance(
             network="default",
             access_configs=[
                 {
-                    "nat_ip": workstation_gpu_ip.address,
+                    "nat_ip": vss_ip.address,
                     "network_tier": "PREMIUM",
                 }
             ],
@@ -48,9 +48,10 @@ workstation_gpu_instance = gcp.compute.Instance(
     ),
     metadata={
         "install-nvidia-driver": "true",
-        "ssh-keys": pixelml_gcp_us_east_1_key_pair.metadata["ssh-keys"],
+        "ssh-keys": pixelml_gcp_eu_west_4_key_pair.metadata["ssh-keys"],
     },
+    tags=["http-server", "https-server"],
     opts=OPTS,
 )
 
-pulumi.export("VM: workstation_gpu: IP", workstation_gpu_ip.address)
+pulumi.export("VM: vss: IP", vss_ip.address)
