71c42964ad2f11e09cf3cc1eeb29b85f49052d6d
tqtensor <victortang212@gmail.com>
2025-05-06 14:55:53 +0000

    chore: stop exporting deprecated vllm and vss resources

--- resources/vm/instance/__init__.py
+++ resources/vm/instance/__init__.py
@@ -1,6 +1,5 @@
 from .n8n import *  # noqa
 from .nextcloud import *  # noqa
 from .tailscale_sg import *  # noqa
-from .vss import *  # noqa
 from .workstation import *  # noqa
 from .workstation_gpu import *  # noqa
