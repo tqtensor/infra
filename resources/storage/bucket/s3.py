797a3c076aa30ab9be2e889277e8d8b948674f39
tqtensor <victortang212@gmail.com>
2025-05-13 06:10:41 +0000

    chore: switch ragflow storage config from aws s3 to minio

--- resources/storage/bucket/s3.py
+++ resources/storage/bucket/s3.py
@@ -2,8 +2,12 @@ import pulumi_aws as aws
 
 from resources.utils import get_options
 
-EC1_OPTS = get_options(profile="personal", region="eu-central-1", type="resource")
-UE1_OPTS = get_options(profile="personal", region="us-east-1", type="resource")
+EC1_OPTS = get_options(
+    profile="personal", region="eu-central-1", type="resource", protect=False
+)
+UE1_OPTS = get_options(
+    profile="personal", region="us-east-1", type="resource", protect=False
+)
 
 
 arq_bucket = aws.s3.Bucket(
@@ -38,11 +42,4 @@ pulumi_bucket = aws.s3.Bucket(
     bucket="tqtensor-pulumi-bucket-us",
     acl="private",
     opts=UE1_OPTS,
-)
-
-ragflow_bucket = aws.s3.Bucket(
-    "ragflow_bucket",
-    bucket="tqtensor-ragflow-bucket-eu",
-    acl="private",
-    opts=EC1_OPTS,
 )
