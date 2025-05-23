b50a1474e8e0021e4d78360e6e0ed4319fb997ea
tqtensor <victortang212@gmail.com>
2025-05-23 12:54:50 +0000

    feat: add MLFlow deployment

--- resources/k8s/helm/__init__.py
+++ resources/k8s/helm/__init__.py
@@ -1,6 +1,7 @@
 from .airbyte import *  # noqa
 from .jupyterhub import *  # noqa
 from .litellm import *  # noqa
+from .mlflow import *  # noqa
 from .n8n import *  # noqa
 from .nginx import *  # noqa
 from .ragflow import *  # noqa
