a926ef6813ccadc3dfbca250d73d073735211014
tqtensor <victortang212@gmail.com>
2025-06-03 05:38:30 +0000

    feat: remove Krypfolio AWS acc

--- resources/providers/aws.py
+++ resources/providers/aws.py
@@ -1,19 +1,5 @@
 import pulumi_aws as aws
 
-# Krypfolio AWS account
-aws_krypfolio_ap_southeast_1 = aws.Provider(
-    "aws-krypfolio-ap-southeast-1", region="ap-southeast-1", profile="krypfolio"
-)
-aws_krypfolio_eu_central_1 = aws.Provider(
-    "aws-krypfolio-eu-central-1", region="eu-central-1", profile="krypfolio"
-)
-aws_krypfolio_eu_north_1 = aws.Provider(
-    "aws-krypfolio-eu-north-1", region="eu-north-1", profile="krypfolio"
-)
-aws_krypfolio_us_east_1 = aws.Provider(
-    "aws-krypfolio-us-east-1", region="us-east-1", profile="krypfolio"
-)
-
 # Personal AWS account
 aws_personal_ap_southeast_1 = aws.Provider(
     "aws-personal-ap-southeast-1", region="ap-southeast-1", profile="personal"
--- resources/vm/networking/__init__.py
+++ resources/vm/networking/__init__.py
@@ -1,4 +1,3 @@
-from .krypfolio_eu_central_1 import *  # noqa
 from .pixelml_eu_central_1 import *  # noqa
 from .pixelml_gcp_eu_west_4 import *  # noqa
 from .pixelml_us_east_1 import *  # noqa
