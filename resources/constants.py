b2dc2b9f6b05c680bb368a41f602b90fe06b369d
tqtensor <victortang212@gmail.com>
2026-01-03 15:58:14 +0000

    chore: refine vscode python settings and add scaleway cluster constant

--- resources/constants.py
+++ resources/constants.py
@@ -4,14 +4,23 @@ Avoids accidental delete of resources or circular dependencies.
 
 import pulumi
 import pulumi_cloudflare as cloudflare
+import pulumiverse_scaleway as scw
 
 # Cloudflare
-tqtensor_com = cloudflare.Zone.get(
+tqtensor_com: cloudflare.Zone = cloudflare.Zone.get(
     "tqtensor_com",
     id="1fa667eb57e3e586d4f0bd8b6cd2e7ad",
 )
 
-tqtensor_homelab_bucket = cloudflare.get_r2_bucket(
-    account_id=pulumi.Config().require("cloudflareAccountId"),
-    bucket_name="tqtensor-homelab",
+tqtensor_homelab_bucket: cloudflare.AwaitableGetR2BucketResult = (
+    cloudflare.get_r2_bucket(
+        account_id=pulumi.Config().require("cloudflareAccountId"),
+        bucket_name="tqtensor-homelab",
+    )
+)
+
+# Scaleway
+par_2_cluster: scw.kubernetes.Cluster = scw.kubernetes.Cluster.get(
+    "par_2_cluster",
+    id="fr-par/773f9f89-6cf5-44d9-b427-667dea748098",
 )
