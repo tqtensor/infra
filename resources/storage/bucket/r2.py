97a9fe29fd1fb42135f54963275bca4042e28ed4
tqtensor <victortang212@gmail.com>
2026-01-09 16:16:15 +0000

    refactor: disable protection and remove tqtensor-reddot R2 bucket

--- resources/storage/bucket/r2.py
+++ resources/storage/bucket/r2.py
@@ -3,7 +3,7 @@ import pulumi_cloudflare as cloudflare
 
 from resources.utils import get_options
 
-OPTS = get_options(provider="cloudflare")
+OPTS = get_options(provider="cloudflare", protect=False)
 
 tqtensor_homelab_bucket = cloudflare.R2Bucket(
     "tqtensor_homelab_bucket",
@@ -12,33 +12,5 @@ tqtensor_homelab_bucket = cloudflare.R2Bucket(
     location="WEUR",
     name="tqtensor-homelab",
     storage_class="Standard",
-    opts=OPTS,
-)
-
-tqtensor_reddot_bucket = cloudflare.R2Bucket(
-    "tqtensor_reddot_bucket",
-    account_id=pulumi.Config().require("cloudflareAccountId"),
-    jurisdiction="default",
-    location="WEUR",
-    name="tqtensor-reddot",
-    storage_class="Standard",
-    opts=OPTS,
-)
-
-tqtensor_reddot_bucket_cors = cloudflare.R2BucketCors(
-    "tqtensor_reddot_bucket_cors",
-    account_id=pulumi.Config().require("cloudflareAccountId"),
-    bucket_name=tqtensor_reddot_bucket.name,
-    rules=[
-        {
-            "allowed": {
-                "methods": ["GET", "PUT", "POST", "DELETE"],
-                "origins": ["chrome-extension://*"],
-                "headers": ["*"],
-            },
-            "max_age_seconds": 3600,
-            "id": "AllowChromeExtension",
-        }
-    ],
     opts=OPTS,
 )
