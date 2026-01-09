e89fd82bfba45e9b92ad5603da3d2e23dab2dd6a
tqtensor <victortang212@gmail.com>
2026-01-09 15:30:13 +0000

    feat: add tqtensor-reddot R2 bucket and API token

--- resources/storage/bucket/r2.py
+++ resources/storage/bucket/r2.py
@@ -14,3 +14,13 @@ tqtensor_homelab_bucket = cloudflare.R2Bucket(
     storage_class="Standard",
     opts=OPTS,
 )
+
+tqtensor_reddot_bucket = cloudflare.R2Bucket(
+    "tqtensor_reddot_bucket",
+    account_id=pulumi.Config().require("cloudflareAccountId"),
+    jurisdiction="default",
+    location="WEUR",
+    name="tqtensor-reddot",
+    storage_class="Standard",
+    opts=OPTS,
+)
