6b1956f8760df73e173488e3fd91fcf53b4c89ab
tqtensor <victortang212@gmail.com>
2026-02-12 08:40:07 +0000

    feat: add smuggler subdomain DNS record and TLS cert

--- resources/cloudflare/record/tqtensor_com.py
+++ resources/cloudflare/record/tqtensor_com.py
@@ -6,31 +6,9 @@ from resources.utils import get_options
 OPTS = get_options(provider="cloudflare")
 
 
-torrent_tqtensor_com = cloudflare.DnsRecord(
-    "torrent_tqtensor_com",
-    name="torrent",
-    ttl=1,
-    type="A",
-    content=nginx_ip_par_2.ip_address,
-    zone_id=tqtensor_com.id,
-    proxied=True,
-    opts=OPTS,
-)
-
-wedding_tqtensor_com = cloudflare.DnsRecord(
-    "wedding_tqtensor_com",
-    name="www.wedding",
-    ttl=1,
-    type="CNAME",
-    content="cname.iwedding.info",
-    zone_id=tqtensor_com.id,
-    proxied=True,
-    opts=OPTS,
-)
-
-paper_tqtensor_com = cloudflare.DnsRecord(
-    "paper_tqtensor_com",
-    name="paper",
+code_tqtensor_com = cloudflare.DnsRecord(
+    "code_tqtensor_com",
+    name="code",
     ttl=1,
     type="A",
     content=nginx_ip_par_2.ip_address,
@@ -50,12 +28,45 @@ paper_ai_tqtensor_com = cloudflare.DnsRecord(
     opts=OPTS,
 )
 
-code_tqtensor_com = cloudflare.DnsRecord(
-    "code_tqtensor_com",
-    name="code",
+paper_tqtensor_com = cloudflare.DnsRecord(
+    "paper_tqtensor_com",
+    name="paper",
     ttl=1,
     type="A",
     content=nginx_ip_par_2.ip_address,
+    zone_id=tqtensor_com.id,
+    proxied=True,
+    opts=OPTS,
+)
+
+smuggler_tqtensor_com = cloudflare.DnsRecord(
+    "smuggler_tqtensor_com",
+    name="smuggler",
+    ttl=1,
+    type="A",
+    content=nginx_ip_par_2.ip_address,
+    zone_id=tqtensor_com.id,
+    proxied=True,
+    opts=OPTS,
+)
+
+torrent_tqtensor_com = cloudflare.DnsRecord(
+    "torrent_tqtensor_com",
+    name="torrent",
+    ttl=1,
+    type="A",
+    content=nginx_ip_par_2.ip_address,
+    zone_id=tqtensor_com.id,
+    proxied=True,
+    opts=OPTS,
+)
+
+wedding_tqtensor_com = cloudflare.DnsRecord(
+    "wedding_tqtensor_com",
+    name="www.wedding",
+    ttl=1,
+    type="CNAME",
+    content="cname.iwedding.info",
     zone_id=tqtensor_com.id,
     proxied=True,
     opts=OPTS,
--- resources/cloudflare/tls/tqtensor_com.py
+++ resources/cloudflare/tls/tqtensor_com.py
@@ -2,22 +2,27 @@ from resources.cloudflare.record.tqtensor_com import (
     code_tqtensor_com,
     paper_ai_tqtensor_com,
     paper_tqtensor_com,
+    smuggler_tqtensor_com,
     torrent_tqtensor_com,
 )
 from resources.cloudflare.tls.utils import create_origin_ca_cert
 
-paper_origin_ca_cert_bundle = create_origin_ca_cert(
-    host=paper_tqtensor_com,
+code_origin_ca_cert_bundle = create_origin_ca_cert(
+    host=code_tqtensor_com,
 )
 
 paper_ai_origin_ca_cert_bundle = create_origin_ca_cert(
     host=paper_ai_tqtensor_com,
 )
 
-torrent_origin_ca_cert_bundle = create_origin_ca_cert(
-    host=torrent_tqtensor_com,
+paper_origin_ca_cert_bundle = create_origin_ca_cert(
+    host=paper_tqtensor_com,
 )
 
-code_origin_ca_cert_bundle = create_origin_ca_cert(
-    host=code_tqtensor_com,
+smuggler_origin_ca_cert_bundle = create_origin_ca_cert(
+    host=smuggler_tqtensor_com,
+)
+
+torrent_origin_ca_cert_bundle = create_origin_ca_cert(
+    host=torrent_tqtensor_com,
 )
